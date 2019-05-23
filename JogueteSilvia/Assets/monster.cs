3a5fbf3040fd7b686f85daa7ea38a2445368fa81
CaioFML <caio.felipemotta@hotmail.com>
2019-05-22 23:54:01 -0300

    Testes NavMesh

---
+++ JogueteSilvia/Assets/monster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class monster : MonoBehaviour
+{
+    public Transform goal;
+    public var victim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victim)
+        {
+            navComponent.SetDestination(targetPoint.postion);
+        }
+    }
+}
